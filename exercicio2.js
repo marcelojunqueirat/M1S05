5b9fd3696e55271373fe55ca9a9251eeb3562bf8
Marcelo Junqueira Teixeira <marcelojxt@gmail.com>
2023-06-14 17:38:10 -0300

    [M1S05] Ex 2

--- exercicio2.js
+++ exercicio2.js
@@ -5,3 +5,33 @@
  * a tradução para a mesma cor em português. Utiliza uma estrutura condicional (switch)
  * para fazer o mapeamento das cores.
  */
+
+let corEmIngles = 'green';
+let corEmPortugues = '';
+
+switch (corEmIngles) {
+  case 'yellow':
+    corEmPortugues = 'amarelo';
+    break;
+  case 'black':
+    corEmPortugues = 'preto';
+    break;
+  case 'blue':
+    corEmPortugues = 'azul';
+    break;
+  case 'red':
+    corEmPortugues = 'vermelho';
+    break;
+  case 'green':
+    corEmPortugues = 'verde';
+    break;
+  case 'brown':
+    corEmPortugues = 'marrom';
+    break;
+
+  default:
+    corEmPortugues = 'inexistente em nosso banco de dados';
+    break;
+}
+
+console.log("A tradução da cor '" + corEmIngles + "' para português é '" + corEmPortugues + "'.");
