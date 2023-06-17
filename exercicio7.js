f1c527b2cd4ebb0d16560d4c2d6a4a5b2bd3666a
Marcelo Junqueira Teixeira <marcelojxt@gmail.com>
2023-06-16 21:51:04 -0300

    [M1S05] Ex 7

--- exercicio7.js
+++ exercicio7.js
@@ -10,3 +10,32 @@
  *      remover os elementos do array compreendidos entre os índices inicial e final. Em seguida, retorna o
  *      array atualizado.
  */
+
+let numeros = [10, 20, 30, 40];
+
+
+let adicionarElemento = (arrayElementos, elemento) => {
+  arrayElementos.push(elemento)
+  return arrayElementos;
+}
+
+let removerUltimoElemento = (arrayElementos) => {
+  elementoRemovido = arrayElementos.pop();
+  return elementoRemovido;
+}
+
+let removerElementosIntervalo = (arrayElementos, inicio, fim) => {
+  arrayElementos.splice(inicio-1, fim);
+  return arrayElementos
+}
+
+console.log(numeros)
+adicionarElemento(numeros, 50)
+adicionarElemento(numeros, 60)
+console.log(numeros)
+
+console.log(removerUltimoElemento(numeros))
+console.log(numeros)
+
+removerElementosIntervalo(numeros, 3, 5)
+console.log(numeros)
