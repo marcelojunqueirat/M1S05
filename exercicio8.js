a1522f2b36fa1fd5576eb68c427234984d91f7d4
Marcelo Junqueira Teixeira <marcelojxt@gmail.com>
2023-06-16 22:04:09 -0300

    [M1S05] Ex 8

--- exercicio8.js
+++ exercicio8.js
@@ -5,3 +5,39 @@
  * matemática. A função `calculadora` executa a operação matemática nos dois números fornecidos e retorna o resultado.
  * Testa a função utilizando diferentes operações como adição, subtração, multiplicação e divisão.
  */
+
+let calculadora = (numero1, numero2, operacao) => {
+  let resultado;
+  switch (operacao) {
+    case '+':
+      resultado = numero1 + numero2;
+      break;
+    case '-':
+      resultado = numero1 - numero2;
+      break;
+    case '/':
+      resultado = numero1 / numero2;
+      break;
+    case '*':
+      resultado = numero1 * numero2;
+      break;
+
+    default:
+      resultado = 'Falha ao processar operação.'
+      break;
+  }
+
+  resultado = (typeof(resultado) === 'number') ? resultado.toFixed(2) : resultado;
+
+  return resultado;
+}
+
+
+console.log(calculadora(105, 108, '+'));
+console.log(calculadora(1005, 506, '-'));
+console.log(calculadora(2000, 11, '/'));
+console.log(calculadora(559.45, 3, '*'));
+
+console.log(calculadora(559.45, 3, '*dd'));
+
+
