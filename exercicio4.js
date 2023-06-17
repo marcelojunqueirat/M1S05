3449a68e30c3d51610ac328af8005beb90776a41
Marcelo Junqueira Teixeira <marcelojxt@gmail.com>
2023-06-16 21:03:38 -0300

    [M1S05] Ex 4

--- exercicio4.js
+++ exercicio4.js
@@ -6,3 +6,18 @@
  * 0, 1, 1, 2, 3, 5, 8, 13, ...
  * Utiliza uma estrutura de repetição (while) para gerar a sequência.
  */
+
+let numero = 300;
+let numeroAnterior = 0;
+let numeroAtual = 1;
+
+console.log(`Sequência de Fibonacci até o numero ${numero}`);
+console.log(numeroAnterior);
+
+while (numeroAtual <= numero) {
+  console.log(numeroAtual);
+  let proximoNumero = numeroAnterior + numeroAtual;
+  numeroAnterior = numeroAtual;
+  numeroAtual = proximoNumero;
+}
+
